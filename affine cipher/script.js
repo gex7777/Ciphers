9e27e3d5b1f21b01b15c9548335fa86fd7e943e2
george menezes <gex7777@gmail.com>
2020-09-27 21:04:35 +0530

    created affine cipher

---
+++ affine cipher/script.js
@@ -0,0 +1,59 @@
+const textElm = document.getElementById("text");
+const alphaElm = document.getElementById("alpha");
+const betaElm = document.getElementById("beta");
+const encryptButton = document.getElementById("encrypt");
+const decryptButton = document.getElementById("decrypt");
+const alphabets = "abcdefghijklmnopqrstuvwxyz";
+const outputElm = document.getElementById("output");
+function encrypt() {
+	//get values from dom
+	var textArray = textElm.value.split("");
+	var alpha = parseInt(alphaElm.value); //convert text values to number before assigning
+	var beta = parseInt(betaElm.value);
+	//convert textArray to numbers
+	var textNumberArray = textArray.map((elm) => {
+		return alphabets.indexOf(elm);
+	});
+	console.log(textNumberArray);
+	//affine enryption
+	var output = textNumberArray.map((elm) => {
+		console.log(elm, alpha, beta);
+		return (elm * alpha + beta) % 26;
+	});
+	output = output.map((elm) => {
+		return alphabets[elm];
+	});
+	output = output.join("").toUpperCase();
+	outputElm.innerText = output;
+}
+
+function decrypt() {
+	//get values from dom
+	var textArray = textElm.value.split("");
+	var alpha = parseInt(alphaElm.value); //convert text values to number before assigning
+	var beta = parseInt(betaElm.value);
+	//convert textArray to numbers
+	var textNumberArray = textArray.map((elm) => {
+		return alphabets.indexOf(elm);
+	});
+	console.log(textNumberArray);
+	var alphaMI = 0;
+	for (var i = 1; i < 26; i++) {
+		if ((alpha * i) % 26 == 1) {
+			alphaMI = i;
+		}
+	}
+	//affine deryption
+	var output = textNumberArray.map((elm) => {
+		console.log(elm, alpha, alphaMI, beta);
+		return (alphaMI * (elm - beta)) % 26;
+	});
+	output = output.map((elm) => {
+		return alphabets[elm];
+	});
+	output = output.join("").toUpperCase();
+	outputElm.innerText = output;
+}
+
+encryptButton.addEventListener("click", encrypt);
+decryptButton.addEventListener("click", decrypt);
