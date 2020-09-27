ce0197c721b1f0b2fe2c669aca25888a2d1725cb
george menezes <gex7777@gmail.com>
2020-09-27 19:23:35 +0530

    fixed hill cipher

--- hill cipher/script.js
+++ hill cipher/script.js
@@ -4,79 +4,77 @@ const alphabets = "abcdefghijklmnopqrstuvwxyz";
 const encryptButton = document.getElementById("encrypt");
 const decryptButton = document.getElementById("decrypt");
 const output = document.getElementById("output");
+function create2dArray(array) {
+	let n = array.length / 2;
+	let Matrics = new Array(n);
+	for (let i = 0; i < Matrics.length; i++) {
+		Matrics[i] = new Array(2);
+	}
+	let count = 0;
+	for (let i = 0; i < n; i++) {
+		for (let j = 0; j < 2; j++) {
+			Matrics[i][j] = array[count++];
+		}
+	}
+	return Matrics.slice();
+}
 function encrypt() {
-	var numberkey = ekey.value.split(",").map((element) => {
+	var key = ekey.value.split(",").map((element) => {
 		return alphabets.indexOf(element) > -1
 			? alphabets.indexOf(element)
 			: parseInt(element);
 	});
-	var plaintextNumber = plaintext.value
+	var messageNumber = plaintext.value
 		.toLowerCase()
 		.split("")
 		.map((element) => {
 			return alphabets.indexOf(element);
 		});
-	console.log(plaintextNumber);
-	var keyMatrics = new Array(plaintextNumber.length);
-	for (let i = 0; i < keyMatrics.length; i++) {
-		keyMatrics[i] = new Array(plaintextNumber.length);
-	}
-	var count = 0;
-	for (let i = 0; i < plaintextNumber.length; i++) {
-		for (let j = 0; j < plaintextNumber.length; j++) {
-			keyMatrics[i][j] = numberkey[count++];
-		}
-	}
+	console.log(messageNumber);
+	var keyMatrics = create2dArray(key);
+	var messageMatrics = create2dArray(messageNumber);
+	console.log(keyMatrics, messageMatrics);
+
 	console.log(keyMatrics);
-
-	var ciphertextNumber = keyMatrics.map((keyRow) => {
-		sum = 0;
-		for (let i = 0; i < plaintextNumber.length; i++) {
-			sum = (sum + keyRow[i] * plaintextNumber[i]) % 26;
-		}
-		return sum;
+	var outputNumber = messageMatrics.map((messageRow) => {
+		return keyMatrics.map((keyRow) => {
+			var sum = 0;
+			for (let i = 0; i < 2; i++) {
+				console.log(messageRow[i], keyRow[i]);
+				sum = (sum + messageRow[i] * keyRow[i]) % 26;
+				console.log(sum);
+			}
+			console.log(sum);
+			return sum;
+		});
 	});
-	var cipherTextArray = ciphertextNumber.map((elm) => {
-		return alphabets[elm];
-	});
-	var cipherText = cipherTextArray.join("").toUpperCase();
+	outputNumber = [].concat(...outputNumber);
 
-	console.log(ciphertextNumber, cipherText);
+	var outputArray = outputNumber.map((number) => {
+		return alphabets[number];
+	});
+	var cipherText = outputArray.join("").toUpperCase();
+
+	console.log(outputNumber, cipherText);
 	output.innerText = cipherText;
 }
 function decrypt() {
-	var numberkey = ekey.value.split(",").map((element) => {
+	var key = ekey.value.split(",").map((element) => {
 		return alphabets.indexOf(element) > -1
 			? alphabets.indexOf(element)
 			: parseInt(element);
 	});
-	var plaintextNumber = plaintext.value
+	var messageNumber = plaintext.value
 		.toLowerCase()
 		.split("")
 		.map((element) => {
 			return alphabets.indexOf(element);
 		});
-	console.log(plaintextNumber);
-	var keyMatrics = new Array(plaintextNumber.length);
-	for (let i = 0; i < keyMatrics.length; i++) {
-		keyMatrics[i] = new Array(plaintextNumber.length);
-	}
-	var count = 0;
-	for (let i = 0; i < plaintextNumber.length; i++) {
-		for (let j = 0; j < plaintextNumber.length; j++) {
-			keyMatrics[i][j] = numberkey[count++];
-		}
-	}
-
-	var determinant =
-		keyMatrics[1][1] * keyMatrics[0][0] - keyMatrics[1][0] * keyMatrics[0][1] <
-		0
-			? -(
-					keyMatrics[1][1] * keyMatrics[0][0] -
-					keyMatrics[1][0] * keyMatrics[0][1]
-			  )
-			: keyMatrics[1][1] * keyMatrics[0][0] -
-			  keyMatrics[1][0] * keyMatrics[0][1];
+	var messageMatrics = create2dArray(messageNumber);
+	var keyMatrics = create2dArray(key);
+	var determin =
+		keyMatrics[1][1] * keyMatrics[0][0] - keyMatrics[1][0] * keyMatrics[0][1];
+	var determinant = determin < 0 ? -determin : determin;
 
 	var transpose = keyMatrics.map((arr) => {
 		return arr.slice();
@@ -87,6 +85,7 @@ function decrypt() {
 	transpose[0][1] = -transpose[0][1] + 26;
 	transpose[1][0] = -transpose[1][0] + 26;
 	var determinantMI = 0;
+
 	for (var i = 1; i < 26; i++) {
 		if ((determinant * i) % 26 == 1) {
 			determinantMI = i;
@@ -98,22 +97,23 @@ function decrypt() {
 			return (elm * determinantMI) % 26;
 		});
 	});
-	console.log(keyMatricsInverse);
-
-	ciphertextNumber = keyMatricsInverse.map((keyRow) => {
-		let sum = 0;
-		for (let i = 0; i < plaintextNumber.length; i++) {
-			sum = (sum + keyRow[i] * plaintextNumber[i]) % 26;
-		}
-		return sum;
+	console.log(keyMatricsInverse, messageMatrics);
+	var outputArray = messageMatrics.map((messageRow) => {
+		return keyMatricsInverse.map((keyRow) => {
+			let sum = 0;
+			for (let i = 0; i < 2; i++) {
+				sum = (sum + keyRow[i] * messageRow[i]) % 26;
+			}
+			console.log(sum);
+			return sum;
+		});
 	});
-	var cipherTextArray = ciphertextNumber.map((elm) => {
+	outputArray = [].concat(...outputArray);
+	var outputTextArray = outputArray.map((elm) => {
 		return alphabets[elm];
 	});
-	var cipherText = cipherTextArray.join("").toUpperCase();
-
-	console.log(ciphertextNumber, cipherText);
-	output.innerText = cipherText;
+	var outputText = outputTextArray.join("").toUpperCase();
+	output.innerText = outputText;
 }
 
 decryptButton.addEventListener("click", decrypt);
